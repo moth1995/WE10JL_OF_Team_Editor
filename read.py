1e09dab6645561dc4ae129409e1a65f90f58474a
moth1995 <marcos-flores@live.com.ar>
2022-01-19 05:59:45 -0300

    Update read.py

--- read.py
+++ read.py
@@ -1,11 +1,11 @@
 from editor.option_file import OptionFile
 
 # Put PES 5 Option File in the same folder as this script
-of_file_location = r"BISLPM-66595JWEXOPT .psu"
+of_file_location = r"test\of3.psu"
 # Load/decrypt the option file
 print("Loading option file...")
 of = OptionFile(of_file_location)
 print("Option file loaded.")
 
-with open("temp.bin", "wb") as binary_file:
+with open("temp3.bin", "wb") as binary_file:
     binary_file.write(of.data)
