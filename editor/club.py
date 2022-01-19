197494687c5e14d2de89826e9a96be5b07e81fdd
moth1995 <marcos-flores@live.com.ar>
2022-01-19 05:58:25 -0300

    Update club.py
    adapted to jlwe10

--- editor/club.py
+++ editor/club.py
@@ -1,5 +1,5 @@
 class Club:
-    total = 208
+    total = 162
     start_address = 680400
     size = 92
     max_name_size = 48
@@ -26,8 +26,10 @@ class Club:
         name_byte_array = self.option_file.data[
             self.name_address : self.name_address + (self.max_name_size + 1)
         ]
-
-        self.name = name_byte_array.partition(b"\0")[0].decode()
+        try:
+            self.name = name_byte_array.partition(b"\0")[0].decode('utf8')
+        except:
+            self.name = f"<Encoding Error Team {self.idx + 64}>"
 
     def update_name(self, name):
         """
@@ -36,7 +38,7 @@ class Club:
         new_name = name[: self.max_name_size]
 
         club_name_bytes = [0] * (self.max_name_size + 1)
-        new_name_bytes = str.encode(new_name)
+        new_name_bytes = str.encode(new_name,"utf-8")
         club_name_bytes[: len(new_name_bytes)] = new_name_bytes
 
         for i, byte in enumerate(club_name_bytes):
