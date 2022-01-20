6a9e0fa95e58c8d7c646664b3a2b66e90f709819
moth1995 <marcos-flores@live.com.ar>
2022-01-20 12:45:27 -0300

    Update main.py
    adding file menu, shop tab, remove save button from swap team tab and fixed bug for when option file is not selected (internal system crash)

--- main.py
+++ main.py
@@ -149,6 +149,189 @@ root.filename=""
 root.filename = filedialog.askopenfilename(initialdir=".",title="Select your option file", filetypes=([("WE10 PS2 Option File", ".psu .xps"),]))
 if root.filename!="":
     of = OptionFile(root.filename)
+
+    # CODE BELOW WAS DONE ONLY FOR DEBUGGING, IF YOU WANT TO FIND THE SHIFT AND MASK FOR A STAT
+    # YOU JUST NEED TO PASS PLAYER IDS THAT YOU WILL USE TO COMPARE AND WRITE THE POSSIBLE VALUES IN THE LIST CALLED TEST
+
+    '''
+    players_ids=[*range(1, 5000, 1)]+[*range(32768, 32952, 1)]
+    all_data=[]
+    for player_id in players_ids:
+        all_data.append(int(get_value(of,player_id,12, 6, 1, "Head overall position")))
+
+
+    #validate=[*range(0, 8, 1)]#+[*range(0, 6, 1)]
+    #validate = [0,1,2,3,4,5,6]
+    #validate = [6,5,4,3,2,1,0]
+    validate = [0, 0, 1, 0, 0, 1, 1, 1]
+    #validate = [63,62,0]
+    print(validate)
+    test=[]
+
+    for shift in range(0,65536):
+        #print (f"the mask is {mask}")
+        for mask in range(0,65536):
+            #if mask==2047:
+            #    print("llegamos al punto conocido")
+            #mask=12
+            offset = 12
+            stat_name = ""
+            test.append((get_value(of,1,offset, shift, mask, stat_name) ))
+            test.append((get_value(of,2,offset, shift, mask, stat_name) ))
+            test.append((get_value(of,3,offset, shift, mask, stat_name) ))
+            
+            test.append((get_value(of,4,offset, shift, mask, stat_name) ))
+            
+            test.append((get_value(of,5,offset, shift, mask, stat_name) ))
+            test.append((get_value(of,6,offset,shift, mask, stat_name) ))
+            
+            
+
+            test.append((get_value(of,7,offset, shift, mask, stat_name) ))
+            test.append((get_value(of,8,offset, shift, mask, stat_name) ))
+            
+            #test.append((get_value(of,690,offset, shift, mask, stat_name) ))
+            #test.append((get_value(of,4473,offset, shift, mask, stat_name) ))
+            #test.append((get_value(of,1485,offset, shift, mask, stat_name) ))
+            
+            #test.append((get_value(of,4521,offset, shift, mask, stat_name) ))
+            #test.append((get_value(of,1229,offset, shift, mask, stat_name) ))
+            #test.append((get_value(of,690,offset, shift, mask, stat_name) ))
+            #test.append((get_value(of,4029,offset, shift, mask, stat_name) ))
+
+            if test == validate:
+                print(shift, mask)
+            test=[]
+    '''
+
+    # Creating a menu for a better and nice gui
+    my_menu=Menu(root)
+    root.config(menu=my_menu)
+    file_menu = Menu(my_menu, tearoff=0)
+    my_menu.add_cascade(label="File", menu=file_menu)
+    file_menu.add_command(label="Save", command=save_btn_action)
+    file_menu.add_command(label="Exit", command=root.quit)
+
+    tabs_container=ttk.Notebook(root)
+    swap_teams_tab=Frame(tabs_container, width=w, height=h)
+    csv_tab=Frame(tabs_container, width=w, height=h)
+    extra_tab=Frame(tabs_container, width=w, height=h)
+    shop_tab = Frame(tabs_container, width=w, height=h)
+    copyright_lbl=Label(root, text="By PES Indie Team")
+    thanks_lbl=Label(root, text="Thanks to PeterC10 for python de/encrypt code for OF")
+
+
+    #Swap teams tab 
+
+    teams_list=national_teams
+    teams_list+=get_of_names(of)
+    csv_team_list = ["---ALL PLAYERS---"] + teams_list
+
+
+
+    team_a_lbl=Label(swap_teams_tab, text="Team A")
+    team_b_lbl=Label(swap_teams_tab, text="Team B")
+    team_a_cmb=ttk.Combobox(swap_teams_tab, state="readonly", value=teams_list, width=30)
+    team_b_cmb=ttk.Combobox(swap_teams_tab, state="readonly", value=teams_list, width=30)
+    swap_kits_check = IntVar()
+    swap_kits_check.set(0)
+    swap_kits_check_btn = Checkbutton(swap_teams_tab, text="Swap OF kits", variable=swap_kits_check)
+    swap_teams_btn=Button(swap_teams_tab, text="Swap teams", command=lambda: swap_btn_action())
+    #save_changes_btn=Button(swap_teams_tab, text="Save changes", command=lambda: save_btn_action())
+
+    #CSV tab
+
+    csv_team_cmb = ttk.Combobox(csv_tab, state="readonly", value=csv_team_list, width=30)
+    csv_team_cmb.current(0)
+    extra_players_check = IntVar()
+    extra_players_check.set(1)
+    extra_players = Checkbutton(csv_tab, text="Include Unused and Edited players", variable=extra_players_check)
+    create_csv_btn = Button(csv_tab, text="Create CSV", command=lambda: export_all_to_csv())
+    import_csv_btn = Button(csv_tab, text="Import CSV", command=lambda: import_all_from_csv())
+
+    #Extra tab
+
+    stat_test_entry = Entry (extra_tab) 
+    test_print_btn=Button(extra_tab, text="Print stat test!", command=lambda: print(all_data[int(stat_test_entry.get())-1]))
+    crypt_lbl=Label(extra_tab, text="Option File cryptology", font = "bold")
+    decrypt_of_btn=Button(extra_tab, text="Decrypt", command=lambda: decrypt_btn_action())
+    encrypt_of_btn=Button(extra_tab, text="Encrypt", command=lambda: encrypt_btn_action())
+    teamform_lbl=Label(extra_tab, text="Formations options", font = "bold")
+    teamform_cmb = ttk.Combobox(extra_tab, state="readonly", value=teams_list, width=30)
+    teamform_cmb.current(0)
+    exp_formation_btn = Button(extra_tab, text="Export team\nformation", command=lambda: export_formation_btn_action())
+    imp_formation_btn = Button(extra_tab, text="Import team\nformation", command=lambda: import_formation_btn_action())
+
+    #Shop tab
+    points=Shop.get_points(of.data)
+    points_lbl = Label(shop_tab, text= f"Please enter a value between 0 and 99999 and press enter\nCurrent points {points}")
+    new_points_box = Entry(shop_tab, width=8)
+    new_points_box.bind('<Return>', lambda event: shop_set_points())
+    unlock_lock_lbl = Label(shop_tab, text= f"Unlock/Lock Shop Items")
+    unlock_shop_btn = Button(shop_tab, text="Unlock shop", command = lambda: messagebox.showinfo(title=appname,message=Shop.unlock_shop(of.data)))
+    lock_shop_btn = Button(shop_tab, text="Lock shop", command = lambda: messagebox.showinfo(title=appname,message=Shop.lock_shop(of.data)))
+    # Since we don't know the name of the background menu we generate some random names
+    bg_list = [f"Main Menu BG {i+1}" for i in range(63)]
+    bg_selector_lbl = Label(shop_tab, text= f"Main Menu Background Selector")
+    bg_selector_cmb = ttk.Combobox(shop_tab, state="readonly", value=bg_list, width=20)
+    bg_selector_cmb.current(Shop.get_background(of.data))
+    bg_selector_btn = Button(shop_tab,text="Set", command=lambda: messagebox.showinfo(title=appname,message=Shop.set_background(of.data,bg_selector_cmb.current())))
+    
+    #Swap team tab placing
+
+    team_a_lbl.place(x=200, y=60)
+    team_b_lbl.place(x=420, y=60)
+    team_a_cmb.place(x=200, y=100)
+    team_b_cmb.place(x=420, y=100)
+    swap_kits_check_btn.place(x=460, y=160)
+    swap_teams_btn.place(x=380, y=160)
+    #save_changes_btn.place(x=376, y=200)
+    copyright_lbl.place(x=0, y=570)
+    thanks_lbl.place(x=480, y=560)
+
+    #CSV tab placing
+
+    csv_team_cmb.place(x=280, y=120)
+    extra_players.place(x=280, y=150)
+    create_csv_btn.place(x=300, y=200)
+    import_csv_btn.place(x=380, y=200)
+    # Extra tab placing
+
+    #stat_test_entry.place(x=200, y=70)
+    #test_print_btn.place(x=200, y=100)
+    teamform_lbl.place(x=280, y=70)
+    teamform_cmb.place(x=280, y=120)
+    exp_formation_btn.place(x=300, y=150)
+    imp_formation_btn.place(x=380, y=150)
+    crypt_lbl.place(x=280,y=220)
+    decrypt_of_btn.place(x=300, y=280)
+    encrypt_of_btn.place(x=380, y=280)
+
+    # Shop tab placing
+
+    points_lbl.place(x=220, y=20)
+    new_points_box.place(x = 320, y = 60)
+    unlock_lock_lbl.place(x = 280, y = 100)
+    unlock_shop_btn.place(x = 260, y = 130)
+    lock_shop_btn.place(x = 360, y = 130)
+    bg_selector_lbl.place(x = 280, y = 170)
+    bg_selector_cmb.place(x = 280, y = 200)
+    bg_selector_btn.place(x = 340, y = 230)
+
+    #Placing tabs and container in the root
+
+    tabs_container.pack()
+    swap_teams_tab.pack(fill="both", expand=1)
+    csv_tab.pack(fill="both", expand=1)
+    extra_tab.pack(fill="both", expand=1)
+    shop_tab.pack(fill="both", expand=1)
+
+    tabs_container.add(swap_teams_tab, text="Swap Teams")
+    tabs_container.add(csv_tab, text="Export/Import CSV")
+    tabs_container.add(extra_tab, text="Extra")
+    tabs_container.add(shop_tab, text="Shop")
+    root.resizable(False, False)
+    root.mainloop()
 else:
     root.destroy()
 
