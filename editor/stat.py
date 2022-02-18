353d5fd5984d55b3586456c793f0b1c70b4c01ca
moth1995 <marcos-flores@live.com.ar>
2022-02-18 06:09:33 -0300

    fix for stats player id checking

--- editor/stat.py
+++ editor/stat.py
@@ -3,7 +3,7 @@ from .utils.common_functions import zero_fill_right_shift
 class Stat:
     def __init__(self, option_file, player,offset, shift, mask, name):
         self.option_file = option_file
-        self.player =player
+        self.player = player
         self.offset = offset
         self.shift = shift
         self.mask = mask
@@ -11,11 +11,12 @@ class Stat:
 
     def get_value(self):
         i = self.player.start_address + 48 + self.player.idx * 124 + self.offset
-        if self.player.idx > self.total_players:
-            i = self.player.start_address_edited + 48 + (self.player.idx - self.first_edited_id) * 124 + self.offset
+        if self.player.idx > self.player.total_players:
+            i = self.player.start_address_edited + 48 + (self.player.idx - self.player.first_edited_id) * 124 + self.offset
         j = (self.option_file.data[i]) << 8 | (self.option_file.data[(i - 1)])
         j = zero_fill_right_shift(j,self.shift)
         j &= self.mask
+        self.normalize(j)
         return j
 
     def set_value(self, new_value):
@@ -30,3 +31,6 @@ class Stat:
         new_value = j | new_value
         self.option_file.data[(i - 1)] = (new_value & 0xFF)
         self.option_file.data[i] = (zero_fill_right_shift(new_value,8))
+
+    def normalize(self,j):
+        pass
