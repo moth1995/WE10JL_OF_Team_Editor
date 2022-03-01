c819ade23fa50b1e8a85491e7902d67840ac39bb
moth1995 <marcos-flores@live.com.ar>
2022-03-01 06:07:59 -0300

    version bump, and bat to clean folder

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo on
-pyinstaller --onefile "main.py" --name "JLWE10_OF_Team_Editor" --noconsole --version-file file_version_info.txt
+pyinstaller --onefile "main.py" --name "JLWE10_OF_Team_Editor_v3" --noconsole --version-file file_version_info.txt
 pause
