88b281acdea1db77cdba45efb949849a8d91fe17
moth1995 <marcos-flores@live.com.ar>
2022-06-19 00:47:30 -0300

    fixing crash on loading when has no bg selector items

--- gui/shop_tab.py
+++ gui/shop_tab.py
@@ -17,7 +17,6 @@ class ShopTab(Frame):
         self.bg_list = [f"Main Menu BG {i+1}" for i in range(self.of.shop.TOTAL_BGS)]
         self.bg_selector_lbl = Label(self, text= f"Main Menu Background Selector")
         self.bg_selector_cmb = Combobox(self, state="readonly", value=self.bg_list, width=20)
-        self.bg_selector_cmb.current(self.of.shop.bg)
         self.bg_selector_btn = Button(self,text="Set", command=lambda: messagebox.showinfo(title=self.appname,message=self.of.shop.set_background(self.bg_selector_cmb.current())))
 
     def shop_set_points(self):
@@ -39,6 +38,7 @@ class ShopTab(Frame):
         self.unlock_shop_btn.place(x = 260, y = 130)
         self.lock_shop_btn.place(x = 360, y = 130)
         if self.of.shop.HAS_BG:
+            self.bg_selector_cmb.current(self.of.shop.bg)
             self.bg_selector_lbl.place(x = 280, y = 170)
             self.bg_selector_cmb.place(x = 280, y = 200)
             self.bg_selector_btn.place(x = 340, y = 230)
